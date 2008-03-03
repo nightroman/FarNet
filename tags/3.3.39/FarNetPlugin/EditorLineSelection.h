cc9856b29010b2178499aa2d6610a7ea9c23122e
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2008-03-03 10:44:03 +0000

    3.3.39

---
+++ tags/3.3.39/FarNetPlugin/EditorLineSelection.h
@@ -0,0 +1,23 @@
+/*
+FAR.NET plugin for Far Manager
+Copyright (c) 2005-2008 FAR.NET Team
+*/
+
+#pragma once
+
+namespace FarNet
+{;
+ref class EditorLineSelection : public ILineSelection
+{
+public:
+	virtual property String^ Text { String^ get(); void set(String^ value); }
+	virtual property int End { int get(); }
+	virtual property int Length { int get(); }
+	virtual property int Start { int get(); }
+	virtual String^ ToString() override;
+internal:
+	EditorLineSelection(int no);
+private:
+	int _no;
+};
+}
