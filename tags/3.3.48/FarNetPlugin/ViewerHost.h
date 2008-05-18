1f93a59b5b959ac9265a80b0e5e76b4f81a44321
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2008-05-18 10:28:39 +0000

    3.3.48

---
+++ tags/3.3.48/FarNetPlugin/ViewerHost.h
@@ -0,0 +1,29 @@
+/*
+FAR.NET plugin for Far Manager
+Copyright (c) 2005-2008 FAR.NET Team
+*/
+
+#pragma once
+#include "Viewer.h"
+
+namespace FarNet
+{;
+ref class ViewerHost
+{
+	ViewerHost() {}
+internal:
+	static array<IViewer^>^ Viewers();
+	static Viewer^ GetCurrentViewer();
+	static int AsProcessViewerEvent(int type, void* param);
+internal:
+	// Viewer waiting for ID
+	static Viewer^ _viewerWaiting;
+	// Any viewer object
+	static BaseViewer _anyViewer;
+private:
+	static void Register(Viewer^ viewer, const ViewerInfo& vi);
+private:
+	// Registered opened viewers
+	static SortedList<int, Viewer^> _viewers;
+};
+}
