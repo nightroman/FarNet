74b757c0ee2a540551eba194db62ca7f9a69e0db
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-03-02 17:40:15 +0000

    4.0.4

---
+++ tags/4.0.4/FarNet/FarNetMan/AnyViewer.h
@@ -0,0 +1,19 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+
+namespace FarNet
+{;
+ref class AnyViewer : IAnyViewer
+{
+public: DEF_EVENT(Closed, _Closed);
+public: DEF_EVENT(GotFocus, _GotFocus);
+public: DEF_EVENT(LosingFocus, _LosingFocus);
+public: DEF_EVENT(Opened, _Opened);
+public:
+	virtual void ViewText(String^ text, String^ title, OpenMode mode);
+};
+}
---
+++ tags/4.0.4/FarNet/FarNetMan/AssemblyInfo.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::CompilerServices;
+using namespace System::Runtime::InteropServices;
+using namespace System::Security::Permissions;
+
+[assembly: AssemblyVersion("4.0.4.0")];
+[assembly: AssemblyTitle("FarNet plugin manager")];
+[assembly: AssemblyDescription("FarNet plugin manager")];
+[assembly: AssemblyConfiguration("")];
+[assembly: AssemblyCompany("http://code.google.com/p/farnet/")];
+[assembly: AssemblyProduct("FarNet")];
+[assembly: AssemblyCopyright("Copyright (c) 2005-2009 FarNet Team")];
+[assembly: AssemblyTrademark("")];
+[assembly: AssemblyCulture("")];
+
+[assembly: ComVisible(false)];
+[assembly: CLSCompliant(true)];
+[assembly: SecurityPermission(SecurityAction::RequestMinimum, UnmanagedCode = true)];
+
+[assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)];
