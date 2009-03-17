dadf7e5878d5a68ec3d0062f9872810ceeb2b6e4
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-03-17 10:43:06 +0000

    4.0.7

---
+++ tags/4.0.7/FarNetMan/MenuItem.h
@@ -0,0 +1,27 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+
+namespace FarNet
+{;
+ref class MenuItem : IMenuItem
+{
+public:
+	DEF_EVENT(OnClick, _OnClick);
+public:
+	virtual property bool Checked;
+	virtual property bool Disabled;
+	virtual property bool IsSeparator;
+	virtual property Object^ Data;
+	virtual property String^ Text;
+	virtual property ToolOptions From;
+public:
+	virtual String^ ToString() override
+	{
+		return Text;
+	}
+};
+}
