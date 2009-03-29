38cfdd7b766c1774730aa8a0d66e28cb4b7db8da
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-03-29 16:15:18 +0000

    4.0.9

---
+++ tags/4.0.9/FarNet/FarNetMan/Wrappers.h
@@ -0,0 +1,40 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+
+class AutoWindowInfo : public WindowInfo
+{
+public:
+	AutoWindowInfo(int index);
+	~AutoWindowInfo();
+private:
+	void operator=(const AutoWindowInfo&) {}
+};
+
+class AutoEditorInfo : public EditorInfo
+{
+public:
+	AutoEditorInfo(bool safe = false);
+	~AutoEditorInfo();
+	void Update();
+private:
+	void operator=(const AutoEditorInfo&) {}
+};
+
+class AutoPluginPanelItem
+{
+public:
+	AutoPluginPanelItem(HANDLE handle, int index, bool selected);
+	~AutoPluginPanelItem();
+	const PluginPanelItem& Get() const { return *m; }
+private:
+	PluginPanelItem* m;
+	char mBuffer[1024];
+	void operator=(const AutoPluginPanelItem&) {}
+};
+
+void GetPanelInfo(HANDLE handle, PanelInfo& info);
+bool TryPanelInfo(HANDLE handle, PanelInfo& info);
