b46329ea5d803e15fd14c8f6dcfefd6f9d3c4704
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-04-06 10:01:08 +0000

    4.0.11

---
+++ tags/4.0.11/FarNetMan/KeyMacroHost.h
@@ -0,0 +1,25 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+
+namespace FarNet
+{;
+ref class KeyMacroHost : public IKeyMacroHost
+{
+public:
+	virtual array<String^>^ GetNames(String^ area);
+	virtual KeyMacroData^ GetData(String^ area, String^ name);
+	virtual void Load();
+	virtual void Post(String^ macro);
+	virtual void Post(String^ macro, bool enableOutput, bool disablePlugins);
+	virtual void Remove(String^ area, String^ name);
+	virtual void Save();
+	virtual void Install(String^ area, String^ name, KeyMacroData^ data);
+	virtual void Install(array<System::Collections::IDictionary^>^ dataSet);
+internal:
+	static KeyMacroHost _instance;
+};
+}
