9c6ffafc77d6a6e652d1c4502769c5a5262ade67
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-04-12 11:55:19 +0000

    4.0.12

---
+++ tags/4.0.12/FarNet/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("4.0.12.0")]
+[assembly: AssemblyTitle("FarNet - .NET interfaces for Far Manager")]
+[assembly: AssemblyDescription("FarNet - .NET interfaces for Far Manager")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
+[assembly: AssemblyProduct("FarNet")]
+[assembly: AssemblyCopyright("Copyright (c) 2005-2009 FarNet Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, UnmanagedCode = true)]
