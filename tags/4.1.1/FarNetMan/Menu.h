ea90baebf48584700b088ef2cdea346452297a4c
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-05-31 16:46:14 +0000

    4.1.1

---
+++ tags/4.1.1/FarNetMan/Menu.h
@@ -0,0 +1,37 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+#include "AnyMenu.h"
+
+namespace FarNet
+{;
+ref class Menu : public AnyMenu, public IMenu
+{
+public:
+	virtual property bool ReverseAutoAssign;
+public:
+	~Menu();
+	!Menu();
+	virtual void Lock();
+	virtual bool Show() override;
+	virtual void Unlock();
+internal:
+	Menu();
+private:
+	FarMenuItemEx* CreateItems();
+	void DeleteItems(FarMenuItemEx* items);
+	int Flags();
+	int* CreateBreakKeys();
+	void ShowMenu(FarMenuItemEx* items, const int* breaks, const wchar_t* title, const wchar_t* bottom, const wchar_t* help);
+	static ToolOptions From();
+private:
+	FarMenuItemEx* _createdItems;
+	int* _createdBreaks;
+	wchar_t* _help;
+	wchar_t* _title;
+	wchar_t* _bottom;
+};
+}
