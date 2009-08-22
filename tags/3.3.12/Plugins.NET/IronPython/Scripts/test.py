337df5f6ad99729c9159fef8b8adb75b5b6543fd
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:10:38 +0000

    old

--- tags/3.3.12/Plugins.NET/IronPython/Scripts/test.py
+++
@@ -1,7 +0,0 @@
-def hello(sender, e):
-	name = ""
-	if e.From == FarManager.OpenFrom.Editor:
-		name = "(Editor)"		
-	far.Msg("Hello, IronPython "+name)
-
-far.RegisterPluginsMenuItem("Hello, IronPython", hello)
