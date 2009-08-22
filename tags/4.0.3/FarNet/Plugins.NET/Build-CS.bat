8e49b106b6ab2e50a60aa6fc45aa64b0f39eae67
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:17:21 +0000

    old

--- tags/4.0.3/FarNet/Plugins.NET/Build-CS.bat
+++
@@ -1,8 +0,0 @@
-
-:: Synopsis : Build FarNet plugin library from CS source
-:: Example  : Build-CS MyPlugin.cs
-:: Output   : MyPlugin.dll
-
-set dotnet=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
-set farlib="%FARHOME%\lib\FarNet.dll"
-%dotnet%\csc /optimize+ /reference:%farlib% /target:library %*
