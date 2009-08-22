786d1b6914ac5f4d7f1943e9fde6971496bb729f
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:17:37 +0000

    old

--- tags/4.0.5/Plugins.NET/Build-VB.bat
+++
@@ -1,8 +0,0 @@
-
-:: Synopsis : Build FarNet plugin library from VB source
-:: Example  : Build-VB MyPlugin.vb
-:: Output   : MyPlugin.dll
-
-set dotnet=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
-set farlib="%FARHOME%\lib\FarNet.dll"
-%dotnet%\vbc /reference:%farlib% /target:library %*
