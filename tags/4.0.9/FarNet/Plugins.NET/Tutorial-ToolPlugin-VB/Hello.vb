4665e49e3c7d76814d87f5d8d812d86e77fe3709
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:18:09 +0000

    old

--- tags/4.0.9/FarNet/Plugins.NET/Tutorial-ToolPlugin-VB/Hello.vb
+++
@@ -1,9 +0,0 @@
-Imports System
-Imports FarNet
-
-Public Class VBTool
-Inherits ToolPlugin
-	Public Overrides Sub Invoke (ByVal sender As Object, ByVal e As ToolEventArgs)
-		Far.Msg("Hello " + Name + " " + e.From.ToString())
-	End Sub
-End Class
