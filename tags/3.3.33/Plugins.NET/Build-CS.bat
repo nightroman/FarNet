75f06e3bb91c317406e6b8178ff9d18ccf7e9ff5
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:13:22 +0000

    old

--- tags/3.3.33/Plugins.NET/Build-CS.bat
+++
@@ -1,9 +0,0 @@
-::
-:: Synopsis : Build Far.Net plugin library from CS source
-:: Example  : Build-CS MyPlugin.cs
-:: Output   : MyPlugin.dll
-::
-
-set dotnet=%WINDIR%\Microsoft.NET\Framework\v2.0.50727
-set farlib="%FARHOME%\lib\FarNetIntf.dll"
-%dotnet%\csc /reference:%farlib% /target:library %*
--- tags/3.3.33/Plugins.NET/MinHello.JS/MinHello.js
+++
@@ -1,9 +0,0 @@
-import FarManager;
-public class HelloJS extends BasePlugin {
-	function item_OnOpen(sender:Object, e:PluginMenuEventArgs) {
-		Far.Msg("Hello world!");
-	}
-	function Connect() {
-		Far.RegisterPluginsMenuItem("Hello js.net", this.item_OnOpen);
-	}
-}
