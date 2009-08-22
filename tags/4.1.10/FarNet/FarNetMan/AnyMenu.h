6478bc8219e2cae313a27b6b033c013ce6854597
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2009-08-22 16:23:55 +0000

    4.1.10

---
+++ tags/4.1.10/FarNet/FarNetMan/AnyMenu.h
@@ -0,0 +1,44 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#pragma once
+#include "MenuItem.h"
+
+namespace FarNet
+{;
+ref class AnyMenu abstract : public IAnyMenu
+{
+public:
+	virtual property bool AutoAssignHotkeys;
+	virtual property bool SelectLast;
+	virtual property bool ShowAmpersands;
+	virtual property bool WrapCursor;
+	virtual property IList<int>^ BreakKeys { IList<int>^ get(); }
+	virtual property IList<IMenuItem^>^ Items { IList<IMenuItem^>^ get(); }
+	virtual property int BreakKey { int get(); }
+	virtual property int MaxHeight;
+	virtual property int Selected { int get(); void set(int value); }
+	virtual property int X { int get(); void set(int value); }
+	virtual property int Y { int get(); void set(int value); }
+	virtual property Object^ SelectedData { Object^ get(); }
+	virtual property Object^ Sender;
+	virtual property String^ Bottom;
+	virtual property String^ HelpTopic;
+	virtual property String^ Title;
+public:
+	virtual bool Show() = 0;
+	virtual IMenuItem^ Add(String^ text);
+	virtual IMenuItem^ Add(String^ text, EventHandler^ handler);
+protected:
+	AnyMenu();
+internal:
+	List<IMenuItem^>^ _items;
+	int _x;
+	int _y;
+	int _selected;
+	List<int> _keys;
+	int _breakKey;
+};
+}
---
+++ tags/4.1.10/FarNet/FarNetMan/CommandLine.cpp
@@ -0,0 +1,111 @@
+/*
+FarNet plugin for Far Manager
+Copyright (c) 2005-2009 FarNet Team
+*/
+
+#include "StdAfx.h"
+#include "CommandLine.h"
+#include "Far.h"
+#include "CommandLineSelection.h"
+
+namespace FarNet
+{;
+ILine^ Far::CommandLine::get()
+{
+	return gcnew FarNet::CommandLine;
+}
+
+ILine^ CommandLine::FullLine::get()
+{
+	return this;
+}
+
+ILineSelection^ CommandLine::Selection::get()
+{
+	return gcnew CommandLineSelection;
+}
+
+int CommandLine::Length::get()
+{
+	int size = Info.Control(INVALID_HANDLE_VALUE, FCTL_GETCMDLINE, 0, 0);
+	return size - 1;
+}
+
+int CommandLine::No::get()
+{
+	return -1;
+}
+
+String^ CommandLine::Eol::get()
+{
+	return String::Empty;
+}
+
+void CommandLine::Eol::set(String^)
+{
+}
+
+String^ CommandLine::Text::get()
+{
+	int size = Info.Control(INVALID_HANDLE_VALUE, FCTL_GETCMDLINE, 0, 0);
+	CBox buf(size);
+	Info.Control(INVALID_HANDLE_VALUE, FCTL_GETCMDLINE, size, (LONG_PTR)(wchar_t*)buf);
+	return gcnew String(buf);
+}
+
+void CommandLine::Text::set(String^ value)
+{
+	if (!value)
+		throw gcnew ArgumentNullException("value");
+
+	PIN_NE(pin, value);
+	if (!Info.Control(INVALID_HANDLE_VALUE, FCTL_SETCMDLINE, 0, (LONG_PTR)pin))
+		throw gcnew OperationCanceledException;
+}
+
+int CommandLine::Pos::get()
+{
+	int pos;
+	if (!Info.Control(INVALID_HANDLE_VALUE, FCTL_GETCMDLINEPOS, 0, (LONG_PTR)&pos))
+		throw gcnew OperationCanceledException;
+	return pos;
+}
+
+void CommandLine::Pos::set(int value)
+{
+	if (value < 0)
+		value = Length;
+
+	if (!Info.Control(INVALID_HANDLE_VALUE, FCTL_SETCMDLINEPOS, value, 0))
+		throw gcnew OperationCanceledException;
+}
+
+void CommandLine::Insert(String^ text)
+{
+	if (!text)
+		throw gcnew ArgumentNullException("text");
+
+	PIN_NE(pin, text);
+	if (!Info.Control(INVALID_HANDLE_VALUE, FCTL_INSERTCMDLINE, 0, (LONG_PTR)pin))
+		throw gcnew OperationCanceledException;
+}
+
+void CommandLine::Select(int start, int end)
+{
+	CmdLineSelect cls;
+	cls.SelStart = start;
+	cls.SelEnd = end;
+	if (!Info.Control(INVALID_HANDLE_VALUE, FCTL_SETCMDLINESELECTION, 0, (LONG_PTR)&cls))
+		throw gcnew OperationCanceledException;
+}
+
+void CommandLine::Unselect()
+{
+	Select(-1, -1);
+}
+
+String^ CommandLine::ToString()
+{
+	return Text;
+}
+}
