7d25bbd040fd90e80cbb6160234488543a126a2f
Roman Kuzmin <nightroman@gmail.com>
2010-01-05 10:09:50 +0000

    4.2.22

--- PowerShellFar/Commands/Find-FarFile.cs
+++ PowerShellFar/Commands/Find-FarFile.cs
@@ -26,19 +26,75 @@ namespace PowerShellFar.Commands
 		/// <summary>
 		/// File name to find.
 		/// </summary>
-		[Parameter(Position = 0, Mandatory = true, HelpMessage = "File name to find.")]
+		[Parameter(Position = 0, Mandatory = true, ParameterSetName = "Name", HelpMessage = "File name to find.")]
 		public string Name { get; set; }
+
+		/// <summary>
+		/// Boolean scriptblock operating on $_ ~ <see cref="FarFile"/>.
+		/// </summary>
+		[Parameter(Position = 0, Mandatory = true, ParameterSetName = "Where", HelpMessage = "Boolean scriptblock operating on $_ ~ FarFile.")]
+		public ScriptBlock Where { get; set; }
+
+		/// <summary>
+		/// Tells to search up, not down.
+		/// </summary>
+		[Parameter(ParameterSetName = "Where", HelpMessage = "Tells to search up, not down.")]
+		public SwitchParameter Up { get; set; }
 
 		///
 		protected override void BeginProcessing()
 		{
-			bool found = A.Far.Panel.GoToName(Name, false);
-			if (!found)
+			if (Name != null)
+			{
+				bool found = A.Far.Panel.GoToName(Name, false);
+				if (!found)
+					WriteError(new ErrorRecord(
+						new FileNotFoundException("File is not found: '" + Name + "'."),
+						"FileNotFound",
+						ErrorCategory.ObjectNotFound,
+						Name));
+			}
+			else
+			{
+				IList<FarFile> files = A.Far.Panel.ShownList;
+				int current = A.Far.Panel.CurrentIndex;
+				int count = files.Count;
+
+				int step;
+				int[] beg;
+				int[] end;
+				if (Up)
+				{
+					step = -1;
+					beg = new int[] { current - 1, count - 1 };
+					end = new int[] { -1, current - 1 };
+				}
+				else
+				{
+					step = 1;
+					beg = new int[] { current + 1, 0 };
+					end = new int[] { count, current + 1 };
+				}
+
+				for (int pass = 0; pass < 2; ++pass)
+				{
+					for (int index = beg[pass]; index != end[pass]; index += step)
+					{
+						SessionState.PSVariable.Set("_", files[index]);
+						if (LanguagePrimitives.IsTrue(Where.InvokeReturnAsIs(null)))
+						{
+							A.Far.Panel.Redraw(index, -1);
+							return;
+						}
+					}
+				}
+
 				WriteError(new ErrorRecord(
-					new FileNotFoundException("File is not found: '" + Name + "'."),
+					new FileNotFoundException("File is not found: {" + Where + "}."),
 					"FileNotFound",
 					ErrorCategory.ObjectNotFound,
-					Name));
+					Where));
+			}
 		}
 
 	}
