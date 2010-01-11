38f08fe9c184fe1fd8d006622b9ea697c994e0e4
Roman Kuzmin <nightroman@users.noreply.github.com>
2010-01-11 11:20:00 +0000

    _100111_111921

--- FarNet/Plugins.NET/Tutorial-ToolPlugin/Build.bat
+++
@@ -1,7 +0,0 @@
-
-rem Build plugin (Hello.dll)
-call ..\Build-CS /out:Hello.dll Hello.cs
-
-rem Build resource files (Hello.resources, Hello.ru.resources)
-"%PROGRAMFILES%\Microsoft SDKs\Windows\v6.0A\bin\ResGen.exe" Hello.restext
-"%PROGRAMFILES%\Microsoft SDKs\Windows\v6.0A\bin\ResGen.exe" Hello.ru.restext
