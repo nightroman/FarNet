aeca8292fd45814c4cfdee456dc37cd9aac09b82
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2010-04-15 10:14:05 +0000

    TrimSaving -> EditorKit

--- trunk/FarNet/Modules/EditorKit/EditorKit.cs
+++ trunk/FarNet/Modules/EditorKit/EditorKit.cs
@@ -1,9 +1,11 @@
 
-// Trims line ends on saving a file.
+// EditorKit: a few editor tools
+// Author: Roman Kuzmin
 
 using FarNet;
 using System;
 
+// Trims line ends on saving a file.
 [System.Runtime.InteropServices.Guid("7a42d03a-83d3-4c8f-b4d3-6483bf1acaf0")]
 [ModuleEditor(Name = TrimSaving.Name)]
 public class TrimSaving : ModuleEditor
@@ -33,5 +35,16 @@ public class TrimSaving : ModuleEditor
 				line.Text = s2;
 		}
 		editor.EndAccess();
+	}
+}
+
+// Sets tabs expansion mode to 'All' for some files.
+[System.Runtime.InteropServices.Guid("c0d595e8-bb33-4724-ade0-4a659488004a")]
+[ModuleEditor(Name = "ExpandTabsAll", Mask = "*.fs;*.fsi;*.fsx;*.fsscript")]
+public class ExpandTabsAll : ModuleEditor
+{
+	public override void Invoke(object sender, ModuleEditorEventArgs e)
+	{
+		((IEditor)sender).ExpandTabs = ExpandTabsMode.All;
 	}
 }
