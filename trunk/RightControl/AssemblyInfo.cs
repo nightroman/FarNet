197e2d3da326377c44afcd690b2facbd1850ac00
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2010-08-13 12:18:56 +0000

    RightControl

---
+++ trunk/RightControl/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyProduct("FarNet.RightControl")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyTitle("FarNet module RightControl")]
+[assembly: AssemblyDescription("Alters editor actions on Ctrl[-Shift]-Left/Right.")]
+[assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
+[assembly: AssemblyCopyright("Copyright (c) 2010 Roman Kuzmin")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
---
+++ trunk/RightControl/Readme.txt
@@ -0,0 +1,18 @@
+
+Module   : FarNet.RightControl
+Release  : 2010.08.13
+Category : Editor
+Author   : Roman Kuzmin
+E-mail   : nightroman@gmail.com
+Source   : http://code.google.com/p/farnet/
+
+	= PREREQUISITES =
+
+ * Far Manager 2.0.1641
+ * Plugin FarNet 4.3.26
+ * .NET Framework 2.0
+
+	= DESCRIPTION =
+
+This editor tool alters default editor actions on Ctrl-Left/Right and
+Ctrl-Shift-Left/Right: go to or select up to the next or previous word bound.
