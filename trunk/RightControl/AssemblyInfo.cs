9518a71b7f867e7f809c4d5f366cdfb0270cc0fd
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2010-08-13 15:49:02 +0000

    RightControl flip

--- trunk/RightControl/AssemblyInfo.cs
+++ trunk/RightControl/AssemblyInfo.cs
@@ -3,9 +3,9 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("FarNet.RightControl")]
-[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
 [assembly: AssemblyTitle("FarNet module RightControl")]
-[assembly: AssemblyDescription("Alters editor actions on Ctrl[-Shift]-Left/Right.")]
+[assembly: AssemblyDescription("Ctrl[-Shift]-Left/Right in editors")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
 [assembly: AssemblyCopyright("Copyright (c) 2010 Roman Kuzmin")]
 
