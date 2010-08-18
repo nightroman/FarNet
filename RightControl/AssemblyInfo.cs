688723bef2b42791e2e42bef284bd10c6598ceb1
Roman Kuzmin <nightroman@gmail.com>
2010-08-18 10:11:15 +0000

    RightControl 1.0.4

--- RightControl/AssemblyInfo.cs
+++ RightControl/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("FarNet.RightControl")]
-[assembly: AssemblyVersion("1.0.3")]
+[assembly: AssemblyVersion("1.0.4")]
 [assembly: AssemblyTitle("FarNet module RightControl")]
 [assembly: AssemblyDescription("Step/Select/Delete by words in editors")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
