a14393f9bb6dbdbf75711797e6d6139947cc103b
Roman Kuzmin <nightroman@gmail.com>
2010-10-06 17:35:55 +0000

    add FarDescription

---
+++ PowerShellFar/Modules/FarDescription/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FarDescription")]
+[assembly: AssemblyDescription("Far Manager file description tools.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
+[assembly: AssemblyProduct("FarDescription")]
+[assembly: AssemblyCopyright("Copyright © 2010 Roman Kuzmin")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(false)]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+static class Res
+{
+	public const string Noun = "FarDescription";
+}
---
+++ PowerShellFar/Modules/FarDescription/UpdateFarDescriptionCommand.cs
@@ -0,0 +1,29 @@
+/*
+PowerShellFar module for Far Manager
+Copyright (c) 2006 Roman Kuzmin
+*/
+
+using System;
+using System.ComponentModel;
+using System.Management.Automation;
+
+namespace FarDescription
+{
+	[Cmdlet(VerbsData.Update, Res.Noun)]
+	[Description("Updates the description file in the directory. If the file becomes empty then it is removed.")]
+	public sealed class UpdateFarDescriptionCommand : PSCmdlet
+	{
+		[Parameter(Position = 0, HelpMessage = "The directory path where the description file is. Default = empty = the process current directory.")]
+		[AllowEmptyString]
+		public string Path { get; set; }
+
+		protected override void BeginProcessing()
+		{
+			if (Path == null)
+				Path = Environment.CurrentDirectory;
+
+			bool exists;
+			Description.UpdateDescriptionFile(Description.GetDescriptionFile(Path, out exists));
+		}
+	}
+}
