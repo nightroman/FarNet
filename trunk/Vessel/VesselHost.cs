efa267b5eeee4d086f2d0328cc69396c53b8a679
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2010-12-28 08:12:41 +0000

    1.0.3

--- trunk/Vessel/VesselHost.cs
+++ trunk/Vessel/VesselHost.cs
@@ -15,6 +15,7 @@ namespace FarNet.Vessel
 	{
 		const string LOG_FILE = "VesselHistory.log";
 		internal const string REG_FACTOR = "Factor";
+		internal const string REG_LIMITS = "Limits";
 
 		/// <summary>
 		/// Gets or sets the disabled flag.
@@ -43,25 +44,114 @@ namespace FarNet.Vessel
 			Far.Net.AnyEditor.Closed += OnEditorClosed;
 		}
 
-		static float _Factor_ = -1;
-		public static float Factor
+		static int? _Factor1_;
+		static int? _Factor2_;
+
+		static void InitFactors()
+		{
+			using (var key = Instance.Manager.OpenRegistryKey(null, false))
+			{
+				if (key == null)
+				{
+					_Factor1_ = 0;
+				}
+				else
+				{
+					var data = ((string)key.GetValue(REG_FACTOR, "0/0")).Split(new char[] { '/' });
+					_Factor1_ = int.Parse(data[0]);
+					_Factor2_ = data.Length > 1 ? int.Parse(data[1]) : 0;
+				}
+			}
+		}
+
+		static public void SetFactors(int factor1, int factor2)
+		{
+			_Factor1_ = factor1;
+			_Factor2_ = factor2;
+			using (var key = Instance.Manager.OpenRegistryKey(null, true))
+				key.SetValue(VesselHost.REG_FACTOR, factor1.ToString() + "/" + factor2.ToString());
+		}
+
+		public static int Factor
 		{
 			get
 			{
-				if (_Factor_ < 0)
-				{
-					using (var key = Instance.Manager.OpenRegistryKey(null, false))
-						if (key != null)
-							_Factor_ = float.Parse((string)key.GetValue(REG_FACTOR, "0"));
-				}
-				return _Factor_;
+				if (!_Factor1_.HasValue)
+					InitFactors();
+
+				return _Factor1_.Value;
+			}
+		}
+
+		public static int Factor2
+		{
+			get
+			{
+				if (!_Factor2_.HasValue)
+					InitFactors();
+
+				return _Factor2_.Value;
+			}
+		}
+
+		static int _Limit0_;
+		static int _Limit1_;
+		static int _Limit2_;
+
+		static void InitLimits()
+		{
+			string text = null;
+			using (var key = Instance.Manager.OpenRegistryKey(null, false))
+			{
+				if (key != null)
+					text = (string)key.GetValue(REG_LIMITS, null);
 			}
 
-			set
+			if (text == null)
 			{
-				_Factor_ = value <= 1 ? 0 : value;
-				using (var key = Instance.Manager.OpenRegistryKey(null, true))
-					key.SetValue(VesselHost.REG_FACTOR, _Factor_.ToString());
+				_Limit0_ = 2;
+				_Limit1_ = 200;
+				_Limit2_ = 30;
+			}
+			else
+			{
+				var data = text.Split(new char[] { '/' });
+				_Limit0_ = int.Parse(data[0]);
+				_Limit1_ = int.Parse(data[1]);
+				_Limit2_ = int.Parse(data[2]);
+			}
+		}
+
+		public static int Limit0
+		{
+			get
+			{
+				if (_Limit0_ == 0)
+					InitLimits();
+
+				return _Limit0_;
+			}
+		}
+
+		public static int Limit1
+		{
+			get
+			{
+				if (_Limit0_ == 0)
+					InitLimits();
+
+				return _Limit1_;
+			}
+		}
+
+		public static int Limit2
+		{
+			get
+			{
+				if (_Limit0_ == 0)
+					InitLimits();
+
+				return _Limit2_;
 			}
 		}
 
@@ -81,7 +171,7 @@ namespace FarNet.Vessel
 				return;
 
 			// go
-			Update(viewer.TimeOfOpen, 0, "view", viewer.FileName);
+			Update(DateTime.Now, 0, "view", viewer.FileName);
 		}
 
 		static void OnEditorClosed(object sender, EventArgs e)
@@ -99,7 +189,7 @@ namespace FarNet.Vessel
 			if (path.EndsWith("?", StringComparison.Ordinal))
 				return;
 
-			Update(editor.TimeOfOpen, editor.KeyCount, (editor.TimeOfSave == DateTime.MinValue ? "edit" : "save"), path);
+			Update(DateTime.Now, editor.KeyCount, (editor.TimeOfSave == DateTime.MinValue ? "edit" : "save"), path);
 		}
 
 		static void Update(DateTime time, int keys, string what, string path)
