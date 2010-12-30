773d2052d97dd4fcaf44650b38e78d4136b000d6
Roman Kuzmin <nightroman@users.noreply.github.com>
2010-12-30 05:05:51 +0000

    Vessel - background and fast training

---
+++ Vessel/Result.cs
@@ -0,0 +1,40 @@
+﻿
+/*
+FarNet module Vessel
+Copyright (c) 2010 Roman Kuzmin
+*/
+
+namespace FarNet.Vessel
+{
+	public class Result
+	{
+		public int Factor1 { get; set; }
+		public int Factor2 { get; set; }
+		public int UpCount { get; set; }
+		public int DownCount { get; set; }
+		public int SameCount { get; set; }
+		public int UpSum { get; set; }
+		public int DownSum { get; set; }
+
+		public int TotalSum
+		{
+			get { return UpSum - DownSum; }
+		}
+
+		public float AverageGain
+		{
+			get
+			{
+				int count = UpCount + DownCount + SameCount;
+				return count == 0 ? 0 : (float)(UpSum - DownSum) / count;
+			}
+		}
+
+		// Keep it float for experiments with float targets
+		public float Target
+		{
+			get { return TotalSum; }
+		}
+
+	}
+}
