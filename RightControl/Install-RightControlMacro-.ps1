cfb6b232ae2817a030294d85623b8f3b465fdda9
Roman Kuzmin <nightroman@users.noreply.github.com>
2011-01-22 19:39:03 +0000

    RC 1.0.9

--- RightControl/Install-RightControlMacro-.ps1
+++ RightControl/Install-RightControlMacro-.ps1
@@ -16,62 +16,47 @@
 	They should be removed when Mantis 1465 is resolved.
 
 	Note: Home/ShiftHome can be used in the common area, too, but this script
-	installs them in the editor only. Smart home is not really useful for one
-	line editors where text normally does not start with spaces.
+	installs them in the editor only. Smart home is not really useful for line
+	editors where text normally does not start with spaces.
 #>
 
 Import-Module FarMacro
 
-function Get-EditorMacro($key)
+function Get-EditorMacro($command)
 {
 @'
-F11
-$If (Menu.Select("RightControl", 3) > 0)
-	Enter
-'@ + " $key" + @'
-
-$Else
-	Esc $AKey
-$End
+callplugin(0xcd, "RightControl:
+'@ + $command + @'
+")
 '@
 }
 
-function Get-CommonMacro($key)
+function Get-CommonMacro($command)
 {
 @'
 $If (AutoCompletion)
 	Esc
 $End
 $If (Editor || Dialog || ((Shell || Info || QView || Tree) && !CmdLine.Empty))
-	F11
-	$If (Menu.Select("RightControl", 3) > 0)
-		Enter
-'@ + " $key" + @'
-
-	$Else
-		Esc $AKey
-	$End
+	callplugin(0xcd, "RightControl:
+'@ + $command + @'
+")
 $Else
 	$AKey
 $End
 '@
 }
 
-function Get-WorkaroundMacro($key)
+function Get-WorkaroundMacro($command)
 {
 @'
 $If (AutoCompletion)
 	Esc
 $End
 $If (Dialog || ((Shell || Info || QView || Tree) && !CmdLine.Empty))
-	F11
-	$If (Menu.Select("RightControl", 3) > 0)
-		Enter
-'@ + " $key" + @'
-
-	$Else
-		Esc $AKey
-	$End
+	callplugin(0xcd, "RightControl:
+'@ + $command + @'
+")
 $Else
 	$AKey
 $End
@@ -81,18 +66,25 @@ $End
 ### Install all macros
 $Far.Macro.Install($(
 	# Editor only
-	New-FarMacro Editor CtrlAltLeft (Get-EditorMacro 7) 'RightControl: vertical left'
-	New-FarMacro Editor CtrlAltRight (Get-EditorMacro 8) 'RightControl: vertical right'
-	New-FarMacro Editor Home (Get-EditorMacro h) 'RightControl: go to smart home'
-	New-FarMacro Editor ShiftHome (Get-EditorMacro s) 'RightControl: select to smart home'
+	New-FarMacro Editor CtrlAltLeft (Get-EditorMacro 'vertical-left') 'RightControl: vertical left'
+	New-FarMacro Editor CtrlAltRight (Get-EditorMacro 'vertical-right') 'RightControl: vertical right'
+	New-FarMacro Editor Home (Get-EditorMacro 'go-to-smart-home') 'RightControl: go to smart home'
+	New-FarMacro Editor ShiftHome (Get-EditorMacro 'select-to-smart-home') 'RightControl: select to smart home'
 	# Common for editor, dialog, cmdline
-	New-FarMacro Common CtrlLeft (Get-CommonMacro 1) 'RightControl: step left'
-	New-FarMacro Common CtrlRight (Get-CommonMacro 2) 'RightControl: step right'
-	New-FarMacro Common CtrlShiftLeft (Get-CommonMacro 3) 'RightControl: select left'
-	New-FarMacro Common CtrlShiftRight (Get-CommonMacro 4) 'RightControl: select right'
-	New-FarMacro Common CtrlBS (Get-CommonMacro 5) 'RightControl: delete left'
-	New-FarMacro Common CtrlDel (Get-CommonMacro 6) 'RightControl: delete right'
+	New-FarMacro Common CtrlLeft (Get-CommonMacro 'step-left') 'RightControl: step left'
+	New-FarMacro Common CtrlRight (Get-CommonMacro 'step-right') 'RightControl: step right'
+	New-FarMacro Common CtrlShiftLeft (Get-CommonMacro 'select-left') 'RightControl: select left'
+	New-FarMacro Common CtrlShiftRight (Get-CommonMacro 'select-right') 'RightControl: select right'
+	New-FarMacro Common CtrlBS (Get-CommonMacro 'delete-left') 'RightControl: delete left'
+	New-FarMacro Common CtrlDel (Get-CommonMacro 'delete-right') 'RightControl: delete right'
 	# Common for dialog, cmdline (workaround)
-	New-FarMacro Common ShiftLeft (Get-WorkaroundMacro 7) 'RightControl: workaround left'
-	New-FarMacro Common ShiftRight (Get-WorkaroundMacro 8) 'RightControl: workaround right'
+	New-FarMacro Common ShiftLeft (Get-WorkaroundMacro 'vertical-left') 'RightControl: workaround left'
+	New-FarMacro Common ShiftRight (Get-WorkaroundMacro 'vertical-right') 'RightControl: workaround right'
 ))
+
+### Show demo macros
+Get-EditorMacro editor
+'-----'
+Get-CommonMacro common
+'-----'
+Get-WorkaroundMacro workaround
