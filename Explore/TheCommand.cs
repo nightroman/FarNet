8b9a4e98718f5c14ad6382ef3ee279a90a6a1315
Roman Kuzmin <nightroman@users.noreply.github.com>
2011-02-20 16:27:01 +0000

    E 1.0.0

---
+++ Explore/TheCommand.cs
@@ -0,0 +1,65 @@
+﻿
+/*
+FarNet module Explore
+Copyright (c) 2010 Roman Kuzmin
+*/
+
+using System;
+using FarNet.Tools;
+
+namespace FarNet.Explore
+{
+	[System.Runtime.InteropServices.Guid("20b46a91-7ef4-4daa-97f5-a1ef291f7391")]
+	[ModuleCommand(Name = "Search in explorer panels", Prefix = "Explore")]
+	public class TheCommand : ModuleCommand
+	{
+		public override void Invoke(object sender, ModuleCommandEventArgs e)
+		{
+			Panel mpanel;
+			if ((mpanel = Far.Net.Panel as Panel) == null || mpanel.Explorer == null)
+			{
+				Far.Net.Message("This is not an explorer panel.");
+				return;
+			}
+
+			// the search
+			var search = new FileSearchExplorer(mpanel.Explorer);
+
+			// parse command, setup the search
+			var command = e.Command.Trim();
+			var tokens = command.Split(new char[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+			foreach (var token in tokens)
+			{
+				if (token.Equals("-Directory", StringComparison.OrdinalIgnoreCase))
+				{
+					search.Directory = true;
+				}
+				else if (token.Equals("-Recurse", StringComparison.OrdinalIgnoreCase))
+				{
+					search.Recurse = true;
+				}
+				else if (search.Process != null)
+				{
+					throw new InvalidOperationException("Invalid command line.");
+				}
+				else
+				{
+					var pattern = token;
+					search.Process = delegate(Explorer explorer, FarFile file)
+					{
+						return Far.Net.MatchPattern(file.Name, pattern);
+					};
+				}
+			}
+
+			// go
+			search.Invoke();
+			if (search.ResultFiles.Count == 0)
+				return;
+
+			// panel
+			var newPanel = search.CreatePanel();
+			newPanel.OpenChild(mpanel);
+		}
+	}
+}
