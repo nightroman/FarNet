1436a8ebb643e69e3b376436aa145b95caf26637
Roman Kuzmin <nightroman@users.noreply.github.com>
2011-05-24 06:27:16 +0000

    RC 1.0.10

--- RightControl/AssemblyInfo.cs
+++ RightControl/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProduct("FarNet.RightControl")]
-[assembly: AssemblyVersion("1.0.11")]
+[assembly: AssemblyVersion("1.0.10")]
 [assembly: AssemblyTitle("FarNet module RightControl")]
 [assembly: AssemblyDescription("Some editor actions work like in other editors")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
-[assembly: AssemblyCopyright("Copyright (c) 2010 Roman Kuzmin")]
+[assembly: AssemblyCopyright("Copyright (c) 2010-2011 Roman Kuzmin")]
 
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
