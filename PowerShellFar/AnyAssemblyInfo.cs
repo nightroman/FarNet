95deea9e8e1be51c7258eb2b223fe87b65b0cf83
Roman Kuzmin <nightroman@gmail.com>
2012-05-13 12:58:01 +0000

    5.0.25

--- PowerShellFar/AnyAssemblyInfo.cs
+++ PowerShellFar/AnyAssemblyInfo.cs
@@ -1,6 +1,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.24.0")]
+[assembly: AssemblyVersion("5.0.25.0")]
 [assembly: AssemblyProduct("PowerShellFar")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
