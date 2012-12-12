1c2630555a3fbfc8feabed212a2377c127348af1
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2012-12-12 02:47:45 +0000

    Macro style

--- trunk/Vessel/Vessel.macro.lua
+++ trunk/Vessel/Vessel.macro.lua
@@ -6,14 +6,18 @@
 	* Common\CtrlShiftF11 - show standard "File view history"
 ]]
 
-Macro { area="Common"; key="AltF11"; flags="DisableOutput"; description="Vessel: Smart file history"; action=function()
+Macro {
+area="Common"; key="AltF11"; flags="DisableOutput"; description="Vessel: Smart file history"; action=function()
 if Plugin.Menu("10435532-9BB3-487B-A045-B0E6ECAAB6BC", "58AD5E13-D2BA-4F4C-82CD-F53A66E9E8C0") then
 	Keys("1")
 else
 	Keys("AKey")
 end
-end;}
+end;
+}
 
-Macro { area="Common"; key="CtrlShiftF11"; description="Vessel: Standard file history"; action=function()
+Macro {
+area="Common"; key="CtrlShiftF11"; description="Vessel: Standard file history"; action=function()
 Keys("AltF11")
-end;}
+end;
+}
