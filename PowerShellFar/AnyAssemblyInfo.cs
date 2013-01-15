4b953b3f3f09685ea1d49622f90d35c0d4d00ed0
Roman Kuzmin <nightroman@gmail.com>
2013-01-15 15:31:40 +0000

    FarNet 5.0.32 and PSF 5.0.35

--- PowerShellFar/AnyAssemblyInfo.cs
+++ PowerShellFar/AnyAssemblyInfo.cs
@@ -1,6 +1,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.34.0")]
+[assembly: AssemblyVersion("5.0.35.0")]
 [assembly: AssemblyProduct("PowerShellFar")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
