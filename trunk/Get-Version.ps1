621034980c1089b9833a445fa2cd160a2e448b02
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2013-12-08 20:42:53 +0000

    PSF 5.0.49 Whole code completion

--- trunk/Get-Version.ps1
+++ trunk/Get-Version.ps1
@@ -2,7 +2,7 @@
 $FarVersion = "3.0.3525"
 $FarNetVersion = "5.0.37"
 $FarNetAccordVersion = "5.0.36"
-$PowerShellFarVersion = "5.0.48"
+$PowerShellFarVersion = "5.0.49"
 
 $ArchiveNames = @(
 	"FarNet.$FarNetVersion.7z"
--- trunk/PowerShellFar/AnyAssemblyInfo.cs
+++ trunk/PowerShellFar/AnyAssemblyInfo.cs
@@ -1,6 +1,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.48.0")]
+[assembly: AssemblyVersion("5.0.49.0")]
 [assembly: AssemblyProduct("PowerShellFar")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
