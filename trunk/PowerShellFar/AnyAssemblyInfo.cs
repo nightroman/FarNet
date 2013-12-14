ba0320665fc0225f0ca65ea2b650b9edd1d31dc6
nightroman <nightroman@adbc6c76-2828-0410-9a29-2134f03e8e62>
2013-12-14 08:30:06 +0000

    PSF 5.0.51

--- trunk/PowerShellFar/AnyAssemblyInfo.cs
+++ trunk/PowerShellFar/AnyAssemblyInfo.cs
@@ -1,6 +1,6 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("5.0.50.0")]
+[assembly: AssemblyVersion("5.0.51.0")]
 [assembly: AssemblyProduct("PowerShellFar")]
 [assembly: AssemblyCompany("http://code.google.com/p/farnet/")]
