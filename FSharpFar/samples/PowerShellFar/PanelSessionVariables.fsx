c148c8216c278b46a06414833bf8262c3b47fc38
Roman Kuzmin <nightroman@gmail.com>
2020-07-07 01:12:03 +0100

    FS - FarNet.FSharp ~ net45

--- FSharpFar/samples/PowerShellFar/PanelSessionVariables.fsx
+++ FSharpFar/samples/PowerShellFar/PanelSessionVariables.fsx
@@ -10,12 +10,8 @@ open FarNet
 open FarNet.FSharp
 
 //
-// Create some variables with known F#, PowerShellFar, FarNet objects.
+// Create variables with some known objects assigned.
 //
-
-// The fsi object, F# compiler settings
-#r @"FarNet\Modules\FSharpFar\FSharp.Compiler.Service.dll"
-let fsi = FSharp.Compiler.Interactive.Shell.Settings.fsi
 
 // The psf object, PowerShellFar
 let psf = (PowerShellFar.invokeScript "$Psf" null).[0]
