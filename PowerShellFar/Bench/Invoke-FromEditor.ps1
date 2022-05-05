09ca23d9f3901ce709f6547bdb2944054a00b70f
Roman Kuzmin <nightroman@gmail.com>
2022-05-05 11:57:08 +0100

    Invoke-FromEditor - run current rule from `.Rule.ps1`

--- PowerShellFar/Bench/Invoke-FromEditor.ps1
+++ PowerShellFar/Bench/Invoke-FromEditor.ps1
@@ -6,22 +6,31 @@
 .Description
 	Saves the editor file and invokes it depending on its type.
 
-	*.build.ps1, *.test.ps1 ~ the current task is invoked by
-	Invoke-Build (https://github.com/nightroman/Invoke-Build)
-	in a new console. Note that built-in [F5] invokes tasks
-	in the Far Manager console and PowerShellFar session.
+	*.build.ps1, *.test.ps1
+	The current task is invoked in a new console by Invoke-Build (https://github.com/nightroman/Invoke-Build).
+	Note that built-in [F5] invokes in the Far Manager console and PowerShellFar session.
 
-	*.Rule.ps1 are invoked by Assert-PSRule (https://github.com/microsoft/PSRule).
-	The input is provided by your command Get-PSRuleInput, e.g. script in the path.
+	*.Rule.ps1
+	The current rule is invoked in a new console by Assert-PSRule (https://github.com/microsoft/PSRule).
+	Input is provided by your own command Get-PSRuleInput, e.g. a script in the path.
 
-	*.ps1 are invoked by powershell.exe
+	*.ps1
+	Other scripts are invoked by powershell in a new console.
+	Note that built-in [F5] invokes in the PowerShellFar session.
 
+	*.md, *.markdown, *.text
 	Markdown files are opened by Show-Markdown-.ps1
 
-	*.bat, *.cmd are invoked by cmd.exe
+	*.bat, *.cmd
+	Batch file are invoked in a new console by cmd.
 
 	Other files are invoked by Invoke-Item.
 #>
+
+trap {
+	Show-FarMessage $_ Invoke-FromEditor -LeftAligned
+	exit
+}
 
 # save, get normalized path and extension
 $editor = $Psf.Editor()
@@ -33,22 +42,44 @@ $ext = [System.IO.Path]::GetExtension($path)
 if ($ext -eq '.ps1') {
 	if ($path -match '\.(?:build|test)\.ps1$') {
 		# Invoke-Build
-		$task = '.'
+		try {
+			$tasks = (Invoke-Build ?? $path).Values
+		}
+		catch {
+			throw "Cannot get tasks from '$path':`n$_"
+		}
+		$name = '.'
 		$line = $editor.Caret.Y + 1
-		foreach($t in (Invoke-Build ?? $path).Values) {
-			if ($t.InvocationInfo.ScriptName -ne $path) {continue}
-			if ($t.InvocationInfo.ScriptLineNumber -gt $line) {break}
-			$task = $t.Name
+		foreach($_ in $tasks) {
+			if ($_.InvocationInfo.ScriptName -ne $path) {continue}
+			if ($_.InvocationInfo.ScriptLineNumber -gt $line) {break}
+			$name = $_.Name
 		}
 		$arg = "-NoExit -NoProfile -ExecutionPolicy Bypass -Command Invoke-Build '{0}' '{1}'" -f @(
-			$task.Replace("'", "''").Replace('"', '\"')
+			$name.Replace("'", "''").Replace('"', '\"')
 			$path.Replace("'", "''")
 		)
 	}
 	elseif ($path -like '*.Rule.ps1') {
 		# PSRule
-		$arg = "-NoExit -NoProfile -ExecutionPolicy Bypass -Command `"Get-PSRuleInput | Assert-PSRule -Path '{0}'`"" -f (
+		try {
+			$rules = @(Get-PSRule $path)
+		}
+		catch {
+			throw "Cannot get rules from '$path':`n$_"
+		}
+		if (!$rules) {
+			return
+		}
+		$name = $rules[0].Name
+		$line = $editor.Caret.Y + 1
+		foreach($_ in $rules) {
+			if ($_.Extent.Line -gt $line) {break}
+			$name = $_.Name
+		}
+		$arg = "-NoExit -NoProfile -ExecutionPolicy Bypass -Command `"Get-PSRuleInput | Assert-PSRule -Path '{0}' -Name '{1}'`"" -f @(
 			$path.Replace("'", "''")
+			$name.Replace("'", "''").Replace('"', '\"')
 		)
 	}
 	else {
@@ -62,7 +93,7 @@ if ($ext -eq '.ps1') {
 }
 
 ### Markdown
-if ('.text', '.md', '.markdown' -contains $ext) {
+if ('.md', '.markdown', '.text' -contains $ext) {
 	Show-Markdown-.ps1
 	return
 }
