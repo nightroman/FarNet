c7f23e05dffe35340e033fccbf20b24b2f8bf0b6
Roman Kuzmin <nightroman@gmail.com>
2025-11-14 17:43:52 +0000

    RightWords net10

--- RightWords/1.build.ps1
+++ RightWords/1.build.ps1
@@ -9,23 +9,23 @@ param(
 )
 
 Set-StrictMode -Version 3
-$ModuleName = 'RightWords'
-$ModuleRoot = "$FarHome\FarNet\Modules\$ModuleName"
-$Description = 'Spell-checker. FarNet module for Far Manager.'
+$_name = 'RightWords'
+$_root = "$FarHome\FarNet\Modules\$_name"
+$_description = 'Spell-checker. FarNet module for Far Manager.'
 
 task build meta, {
-	exec { dotnet build -c $Configuration /p:FarHome=$FarHome }
+	exec { dotnet build -c $Configuration /p:FarHome=$FarHome --tl:off }
 }
 
 task publish help, resgen
 
-task help -Inputs README.md -Outputs $ModuleRoot\RightWords.hlf {
+task help -Inputs README.md -Outputs $_root\RightWords.hlf {
 	exec { pandoc.exe README.md "--output=$env:TEMP\z.html" --from=gfm --syntax-highlighting=none }
-	exec { HtmlToFarHelp.exe "from=$env:TEMP\z.html" "to=$ModuleRoot\RightWords.hlf" }
+	exec { HtmlToFarHelp.exe "from=$env:TEMP\z.html" "to=$_root\RightWords.hlf" }
 }
 
 # https://github.com/nightroman/PowerShelf/blob/main/Invoke-Environment.ps1
-task resgen -Partial -Inputs RightWords.restext, RightWords.ru.restext -Outputs $ModuleRoot\RightWords.resources, $ModuleRoot\RightWords.ru.resources {
+task resgen -Partial -Inputs RightWords.restext, RightWords.ru.restext -Outputs $_root\RightWords.resources, $_root\RightWords.ru.resources {
 	begin {
 		$VsDevCmd = @(Get-Item "$env:ProgramFiles\Microsoft Visual Studio\2022\*\Common7\Tools\VsDevCmd.bat")
 		Invoke-Environment.ps1 -File ($VsDevCmd[0])
@@ -40,18 +40,18 @@ task clean {
 }
 
 task version {
-	($Script:Version = Get-BuildVersion History.txt '^= (\d+\.\d+\.\d+) =$')
+	($Script:_version = Get-BuildVersion History.txt '^= (\d+\.\d+\.\d+) =$')
 }
 
-task meta -Inputs .build.ps1, History.txt -Outputs Directory.Build.props -Jobs version, {
+task meta -Inputs $BuildFile, History.txt -Outputs Directory.Build.props -Jobs version, {
 	Set-Content Directory.Build.props @"
 <Project>
 	<PropertyGroup>
-		<Description>$Description</Description>
 		<Company>https://github.com/nightroman/FarNet</Company>
 		<Copyright>Copyright (c) Roman Kuzmin</Copyright>
-		<Product>FarNet.$ModuleName</Product>
-		<Version>$Version</Version>
+		<Description>$_description</Description>
+		<Product>FarNet.$_name</Product>
+		<Version>$_version</Version>
 		<IncludeSourceRevisionInInformationalVersion>False</IncludeSourceRevisionInInformationalVersion>
 	</PropertyGroup>
 </Project>
@@ -73,24 +73,18 @@ task markdown {
 
 task package markdown, version, {
 	remove z
-	$toModule = mkdir "z\tools\FarHome\FarNet\Modules\$ModuleName"
+	$toModule = New-Item -ItemType Directory "z\tools\FarHome\FarNet\Modules\$_name"
 
-	# module
-	exec { robocopy $ModuleRoot $toModule /s /xf *.pdb } 1
+	Copy-Item -Destination $toModule -Recurse `
+	$_root\*,
+	..\LICENSE,
+	README.html,
+	History.txt,
+	RightWords.macro.lua
 
-	# meta
-	Copy-Item -Destination z @(
-		'README.md'
-		'..\Zoo\FarNetLogo.png'
-	)
-
-	# module
-	Copy-Item -Destination $toModule @(
-		"README.html"
-		"History.txt"
-		"RightWords.macro.lua"
-		"..\LICENSE"
-	)
+	Copy-Item -Destination z `
+	..\Zoo\FarNetLogo.png,
+	README.md
 
 	Assert-SameFile.ps1 -Result (Get-ChildItem $toModule -Recurse -File -Name) -Text -View $env:MERGE @'
 History.txt
@@ -99,6 +93,7 @@ README.html
 RightWords.dll
 RightWords.hlf
 RightWords.macro.lua
+RightWords.pdb
 RightWords.resources
 RightWords.ru.resources
 WeCantSpell.Hunspell.dll
@@ -106,21 +101,21 @@ WeCantSpell.Hunspell.dll
 }
 
 task nuget package, version, {
-	equals $Version (Get-Item "$ModuleRoot\$ModuleName.dll").VersionInfo.ProductVersion
+	equals $_version (Get-Item "$_root\$_name.dll").VersionInfo.ProductVersion
 
 	Set-Content z\Package.nuspec @"
 <?xml version="1.0"?>
 <package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
 	<metadata>
 		<id>FarNet.RightWords</id>
-		<version>$Version</version>
+		<version>$_version</version>
 		<authors>Roman Kuzmin</authors>
 		<owners>Roman Kuzmin</owners>
 		<projectUrl>https://github.com/nightroman/FarNet</projectUrl>
 		<icon>FarNetLogo.png</icon>
 		<readme>README.md</readme>
 		<license type="expression">BSD-3-Clause</license>
-		<description>$Description</description>
+		<description>$_description</description>
 		<releaseNotes>https://github.com/nightroman/FarNet/blob/main/RightWords/History.txt</releaseNotes>
 		<tags>FarManager FarNet Module Hunspell</tags>
 	</metadata>
