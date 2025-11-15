c18a1c8c22cb2fb4427cc74f134af90fd962be0a
Roman Kuzmin <nightroman@gmail.com>
2025-11-15 05:14:29 +0000

    PS pwsf

--- PowerShellFar/Modules/pwsf/Program.cs
+++ PowerShellFar/Modules/pwsf/Program.cs
@@ -3,24 +3,68 @@ using System.Text;
 
 const string FarExe = "Far.exe";
 
-// executable path
+// executable
 var baseDirectory = AppContext.BaseDirectory;
 var fileName = $"{baseDirectory}\\{FarExe}";
 
-// make arguments
-var sb = new StringBuilder("/set:Panel.Left.Visible=false /set:Panel.Right.Visible=false");
-var pwd = args.Length > 0 ? args[0] : Environment.CurrentDirectory;
-if (pwd is { })
-    sb.Append(" \"{pwd}\" \"{pwd}\"");
+// arguments
+
+string pwd;
+int st;
+if (args.Length > 0 && !args[0].StartsWith("-") && !args[0].StartsWith("/"))
+{
+	pwd = args[0].TrimEnd('/').TrimEnd('\\');
+	if (pwd.EndsWith(":"))
+		pwd += "\\\\";
+	st = 1;
+}
+else
+{
+	pwd = Environment.CurrentDirectory;
+	st = 0;
+}
+
+var sb = new StringBuilder($"\"{pwd}\" \"{pwd}\" -set:Panel.Left.Visible=false -set:Panel.Right.Visible=false");
+for (int i = st; i < args.Length; i++)
+{
+	var arg = args[i];
+	if (arg.StartsWith("-") || arg.StartsWith("/"))
+	{
+		var str = arg.TrimStart('-', '/');
+
+		if (str.Equals("ro", StringComparison.OrdinalIgnoreCase) ||
+			str.Equals("ro-", StringComparison.OrdinalIgnoreCase))
+		{
+			sb.Append($" {arg}");
+			continue;
+		}
+
+		if (str.StartsWith("set:", StringComparison.OrdinalIgnoreCase))
+		{
+			sb.Append($" \"{arg}\"");
+			continue;
+		}
+	}
+
+	Console.WriteLine("""
+		Usage: pwsf [<path>] [<arguments>]
+		Arguments:
+		-ro[-]
+		-set:<parameter>=<value>
+		""");
+
+	return 1;
+}
+var arguments = sb.ToString();
 
 // trigger
 Environment.SetEnvironmentVariable("FAR_START_COMMAND", "ps:$Psf.StartCommandConsole()");
 
 var process = Process.Start(new ProcessStartInfo
 {
-    UseShellExecute = false,
-    FileName = fileName,
-    Arguments = sb.ToString(),
+	UseShellExecute = false,
+	FileName = fileName,
+	Arguments = arguments,
 });
 
 process!.WaitForExit();
